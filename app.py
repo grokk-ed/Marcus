6667746cd26815555006e08f2774aa4afd964fc8
Hai <hai@firstresonance.io>
2020-04-07 20:06:43 -0700

    finish initial setup

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from models import User  # noqa: E402, F401
 
 
 @app.route('/')
